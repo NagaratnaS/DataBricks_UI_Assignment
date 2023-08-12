600a8a93bb240a459f4737ec37bd6618cb8e374d
NagaratnaS <hegdenagaratna34@gmail.com>
2023-08-12 17:55:09 +0530

    set the height of root element

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ComparisonContainer from "./ComparisonContainer/ComparisonContainer";
 
 function App() {
   return (
-    <div style = {{ backgroundColor : '#f6f4f4' , height: 'auto' }}>
+    <div style = {{ backgroundColor : '#f6f4f4' , minHeight: '100vh' , maxHeight: '1000vh'}}>
       {/* <SideNavBar/> */}
       <Header/>
       <ComparisonContainer/>
