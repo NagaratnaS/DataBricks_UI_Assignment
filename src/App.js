1d784239143afd9a28b78d13249e5069d7254ddb
NagaratnaS <hegdenagaratna34@gmail.com>
2023-08-12 19:29:44 +0530

    Added navigation Bar and box shadow for header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Header from "./Header/Header";
+import SideNavBar from "./SideNavBar/SideNavBar";
 import ComparisonContainer from "./ComparisonContainer/ComparisonContainer";
 
 function App() {
   return (
     <div style = {{ backgroundColor : '#f6f4f4' , minHeight: '100vh' , maxHeight: '1000vh'}}>
-      {/* <SideNavBar/> */}
-      <Header/>
-      <ComparisonContainer/>
+
+        <div style={{display: 'block'}}>
+          <SideNavBar/>
+          <Header/>
+        </div>
+        
+        <ComparisonContainer/>
+      
     </div>
   );
 }
