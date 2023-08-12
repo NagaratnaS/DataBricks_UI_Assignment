3661086993924ac8bd7d2e6054ba50e36e1093b7
NagaratnaS <hegdenagaratna34@gmail.com>
2023-08-12 23:13:15 +0530

    added navigation bar icons

--- src/SideNavBar/SideNavBar.js
+++ src/SideNavBar/SideNavBar.js
@@ -1,5 +1,13 @@
 import React from "react";
 import './SideNavBar.css';
+import HomeIcon from '../Images/HomeIcon.png';
+import PythonIcon from '../Images/PythonIcon.png';
+import RIcon from '../Images/RIcon.png';
+import JavaIcon from '../Images/JavaIcon.png';
+import CalenderIcon from '../Images/CalenderIcon.png';
+import MessageIcon from '../Images/MessageIcon.png';
+import LogoutIcon from '../Images/LogoutIcon.png';
+
 
 
 export default function SideNavBar(){
@@ -7,16 +15,23 @@ export default function SideNavBar(){
 
     return(
             <div>
-                <header>CX</header>
-                <ul>
-                <li><a href="#"><i class="fas fa-qrcode"></i>Dashboard</a></li>
-                <li><a href="#"><i class="fas fa-link"></i>Shortcuts</a></li>
-                <li><a href="#"><i class="fas fa-stream"></i>Overview</a></li>
-                <li><a href="#"><i class="fas fa-calendar-week"></i>Events</a></li>
-                <li><a href="#"><i class="far fa-question-circle"></i>About</a></li>
-                <li><a href="#"><i class="fas fa-sliders-h"></i>Services</a></li>
-                <li><a href="#"><i class="far fa-envelope"></i>Contact</a></li>
+                <div className="nav_style">
+                <ul className="list_style">
+                    <li className="text_style">CX</li>
+                    <div className="list_element">
+                        <li><img src={HomeIcon} className="home"></img></li>
+                        <li><img src={PythonIcon} className="common_style"></img></li>
+                        <li><img src={RIcon} className="common_style"></img></li>
+                        <li><img src={JavaIcon} className="common_style"></img></li>
+                        <li><img src={CalenderIcon} className="common_style"></img></li>
+                        <li><img src={MessageIcon} className="message"></img></li>
+                        <li><img src={LogoutIcon} className="exit"></img></li>
+                    </div>
+                    
+
                 </ul>
+                </div>
+                
             </div>
     );
 }
